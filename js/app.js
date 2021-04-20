c068c635d59d0872ce01a815f52eddeb9ca88299
Dakota Nelson <kotalilyy@gmail.com>
2021-04-20 06:55:16 -0500

    loop through elements in array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // DOM objects
-const mainScreen = document.querySelector('.main-screen');
+const mainScreen = document.querySelector(".main-screen");
 const pokeName = document.querySelector(".poke-name");
 const pokeId = document.querySelector(".poke-id");
 const pokeFrontImage = document.querySelector(".poke-front-image");
@@ -9,15 +9,60 @@ const pokeTypeTwo = document.querySelector(".poke-type-two");
 const pokeWeight = document.querySelector(".poke-weight");
 const pokeHeight = document.querySelector(".poke-height");
 
-console.log(pokeName);
+// constants and variables
+const TYPES = [
+  "normal",
+  "fighting",
+  "flying",
+  "poison",
+  "ground",
+  "rock",
+  "bug",
+  "ghost",
+  "steel",
+  "fire",
+  "water",
+  "grass",
+  "electric",
+  "psychic",
+  "ice",
+  "dragon",
+  "dark",
+  "fairy",
+];
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+// functions
+const resetScreen = () => {
+  mainScreen.classList.remove("hide");
+  for (const type of TYPES) {
+    mainScreen.classList.remove(type);
+  }
+};
+
+fetch("https://pokeapi.co/api/v2/pokemon/4")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    mainScreen.classList.remove('hide');
-    pokeName.textContent = data['name'];
-    pokeId.textContent = data['id'];
-    pokeWeight.textContent = data['weight'];
-    pokeHeight.textContent = data['height'];
+
+    resetScreen();
+
+    const dataTypes = data["types"];
+    const dataFirstType = dataTypes[0];
+    const dataSecondType = dataTypes[1];
+    pokeTypeOne.textContent = dataFirstType["type"]["name"];
+    if (dataSecondType) {
+      pokeTypeTwo.classList.remove("hide");
+      pokeTypeTwo.textContent = dataSecondType["type"]["name"];
+    } else {
+      pokeTypeTwo.classList.add("hide");
+      pokeTypeTwo.textContent = "";
+    }
+    mainScreen.classList.add(dataFirstType["type"]["name"]);
+
+    pokeName.textContent = data["name"];
+    pokeId.textContent = data["id"];
+    pokeWeight.textContent = data["weight"];
+    pokeHeight.textContent = data["height"];
+    pokeFrontImage.src = data["sprites"]["front_default"] || "";
+    pokeBackImage.src = data["sprites"]["back_default"] || "";
   });
